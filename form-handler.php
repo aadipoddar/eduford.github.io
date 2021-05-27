de54f2ec29a82e1ac6ac4ba51e8636ed1fe189b1
aadipoddar <83405769+aadipoddar@users.noreply.github.com>
2021-05-27 15:43:20 +0530

    changed the email from
    changed it to info@https://aadipoddar.github.io/eduford.github.io/index.html

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'info@https://aadipoddar.github.io/eduford.github.io/index.html';
 
 $email_subject = 'New Form Submission';
 
