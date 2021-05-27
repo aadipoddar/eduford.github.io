ad59fb07df3f2a1aa1561ab09afdef1cad741384
aadipoddar <83405769+aadipoddar@users.noreply.github.com>
2021-05-27 15:44:23 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@https://aadipoddar.github.io/eduford.github.io/index.html';
+$email_from = 'https://aadipoddar.github.io/eduford.github.io/index.html';
 
 $email_subject = 'New Form Submission';
 
